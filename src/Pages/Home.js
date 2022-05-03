15b5c8e4e128bd01b1648369c4b028cfa56d10c5
canosaer <canosaer@gmail.com>
2022-05-03 13:31:38 -0400

    intro animation

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,44 +1,99 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { Component, useState, useEffect, useRef } from 'react'
 import LogoCube from '../components/LogoCube'
 import {useWindowDimensions} from '../utilities'
-import gsap from 'gsap'
 
-export default function Home() {
-    const canvas = useRef();
-    const q = gsap.utils.selector(canvas);
-    const tl = useRef();
-    const width = useWindowDimensions().width;
-    const whiteLines = [useRef(),useRef(),useRef(),useRef(),useRef(),useRef(),useRef(),useRef(),useRef(),useRef(),useRef(),useRef(),useRef(),useRef(),useRef(),useRef(),useRef(),useRef()]
+import FluidAnimation from "../react-fluid-animation";
+import random from "random";
+
+
+const defaultConfig = {
+    textureDownsample: 1,
+    densityDissipation: 1,
+    velocityDissipation: 0.99,
+    pressureDissipation: 0.93,
+    pressureIterations: 25,
+    curl: 30,
+    splatRadius: 0.015
+};
+
   
-    // useEffect(() => {       
-        // www.youtube.com/watch?v=PNL9DaZgHpU
-    //     console.log(width)     
-    //     tl.current = gsap.timeline()
-    //         .from(q(".circle1"), {
-    //             x: width,
-    //             duration: 5,
-    //         })
-    // }, []);
-    
+export default class Home extends Component {
+state = {
+    config: {
+    ...defaultConfig
+    }
+};
+
+componentDidMount() {
+    this._reset();
+}
+
+render() {
+    const { config } = this.state;
+
     return (
-        <>
-            <section className="home" ref={canvas}>
-                <LogoCube />
-                <div className="white-circle">
-                    {/* {whiteLines.map((line, i) => {
-                        const key = `white-line--${i}`
-                        const rotation = i*20
-                        const style = {
-                            transform: `rotate(${rotation}deg`,
+    <div
+        style={{
+        height: "100vh",
+        pointerEvents: "none"
+        }}
+    >
+        <FluidAnimation config={config} animationRef={this._animationRef} />
 
-                        }
+        <div
+            style={{
+                position: "absolute",
+                zIndex: 1,
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                padding: "1em",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                pointerEvents: "none",
+            }}
+        >
+            <h1 className="home__heading">Eric Canosa</h1>
+        </div>
+    </div>
+    );
+}
 
-                        return(
-                            <div key={key} className="white-line" style={style}></div>
-                        )
-                    })} */}
-                </div>
-            </section>
-        </>
-    )
+_animationRef = ref => {
+    this._animation = ref;
+    this._reset();
+};
+
+_onUpdate = config => {
+    this.setState({ config });
+};
+
+_onClickRandomSplats = () => {
+    this._animation.addSplats((5 + Math.random() * 20) | 0);
+};
+
+_onReset = () => {
+    this.setState({ config: { ...defaultConfig } });
+};
+
+_reset() {
+    if (this._animation && window.innerWidth < 455) {
+        this._animation.addRandomSplats(random.int(3, 5));
+    }
+    else if (this._animation && window.innerWidth < 768) {
+        this._animation.addRandomSplats(random.int(3, 7));
+    }
+    else if(this._animation && window.innerWidth < 1200){
+        this._animation.addRandomSplats(random.int(8, 9));
+    }
+    else if(this._animation && window.innerWidth < 1700){
+        this._animation.addRandomSplats(random.int(10, 19));
+    }
+    else if(this._animation){
+        this._animation.addRandomSplats(random.int(10, 24));
+    }
+}
 }
