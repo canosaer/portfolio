3162a71577fcdb4df1a505e97ba0618ea0df17f2
canosaer <67963828+canosaer@users.noreply.github.com>
2022-05-13 13:23:59 -0400

    more

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -6,16 +6,11 @@ export default function About() {
 
     return (
         <section className="about">
-            <div className="about__heading-row about__heading-row_mobile">
-                <h2 className="about__heading">Eric Canosa | Creative Technologist</h2>
-            </div>
-            <div className="about__heading-row about__heading-row_desktop">
-                <h2 className="about__heading">Eric Canosa</h2>
-                <h3 className="about__subheading">Creative Technologist</h3>
-            </div>
+            <h2 className="about__heading">Eric Canosa</h2>
+            <h3 className="about__subheading">Creative Technologist</h3>
             <img className="about__image" src={profile} alt="eric canosa headshot" />
             <p className="about__text">
-                I first got into computers with the release of DOOM in 1993 and it has been an awesome ride ever since. I've dabbled with hardware and networks, but my passion is developing user experiences that create novel opportunities for people to work, learn, and play. I'm looking to take my creative technology skills to the next level at a company where I can stay at the cutting edge and make an impact.
+                I first got into computers with the release of DOOM in 1993 and it has been an awesome ride ever since. I've dabbled with hardware and networks, but my passion is developing user experiences that create novel opportunities for people to work, learn, and play.
             </p>
             <div className="about__links">
                 <a className="about__link about__link_linkedin" href="https://www.linkedin.com/in/eric-canosa/" target="_blank">< FontAwesomeIcon className="about__icon about__icon_linkedin" icon={["fab", "linkedin-in"]} /> <p className="about__tooltip about__tooltip_linkedin">linkedin</p></a>
