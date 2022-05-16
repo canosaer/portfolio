245f738e4c1f508268a5b2ca197faa2404e99957
canosaer <canosaer@gmail.com>
2022-05-16 15:50:14 -0400

    more

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -46,14 +46,14 @@ export default function Projects() {
 
                     if(i >= firstVisible && i < firstVisible+increment){
                         return(
-                            <a href={project.url} rel="noreferrer" target="_blank" className="projects__card" key={key}>
-                                <h3 className="projects__subheading">{project.name}</h3>
+                            <article className="projects__card" key={key}>
+                                <a className="projects__subheading" href={project.url} rel="noreferrer" target="_blank">{project.name}</a>
                                 <p className="projects__caption">{project.desc}</p>
                                 <div className="projects__link-row">
                                     <a className="projects__link" href={project.url} rel="noreferrer" target="_blank">View Site</a>
                                     <a className="projects__link" href={project.repo} rel="noreferrer" target="_blank">View Code</a>
                                 </div>
-                            </a>
+                            </article>
                         )
                     }
                 })}
