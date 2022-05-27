836db632d03ac26f096508451a2bd84eca908112
canosaer <67963828+canosaer@users.noreply.github.com>
2022-05-27 14:28:07 -0400

    Update Contact.js

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -7,18 +7,22 @@ export default function Contact() {
 
     const [subject, setSubject] = useState('')
     const [message, setMessage] = useState('')
-
-    const parts = ["@", 'com', 'canosaer', 'gmail', ".", ]
+    const [parts, setParts] = useState([])
 
     const ContactSchema = Yup.object().shape({
         subject: Yup.string().required('Required'),
         message: Yup.string().required('Required'),
       });
 
+    const handleMail = (enabled) => {
+        if(enabled) setParts(["@", 'com', 'canosaer', 'gmail', ".", ])
+        else setParts([])
+    }
+
     return (
         <section className="contact">
             <h1 className="contact__heading">Contact Me</h1>
-            <h2 classNAme="contact__subjeading">You can email me at canosaer</h2>
+            <h2 className="contact__subjeading">You can email me at <span className="contact__at">&#64;</span></h2>
             <Formik
                 initialValues={{
                     subject: '',
@@ -35,6 +39,8 @@ export default function Contact() {
                         <Field value={message} onChange={(e) => setMessage(e.target.value)} className="contact-form__input contact-form__input_message" name="message" />
                         <div className={errors.message && touched.message ? "contact-form__error contact-form__error_message" : "contact-form__error contact-form__error_message transparent"}>{errors.message}</div>
                         <a
+                            onMouseEnter={ () => handleMail(true) }
+                            onMouseLeave={ () => handleMail(false) }
                             href={`mailto:${parts[2]}${parts[0]}${parts[3]}${parts[4]}${parts[1]}?subject=${subject}&body=${message}`} 
                             className="contact-form__button">
                                 Submit
