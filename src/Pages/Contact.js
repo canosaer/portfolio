7f625afd35ef9b3322da3cd2fb9768b448b2853b
canosaer <67963828+canosaer@users.noreply.github.com>
2022-05-27 16:15:36 -0400

    more

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 
@@ -21,33 +22,33 @@ export default function Contact() {
 
     return (
         <section className="contact">
-            <h1 className="contact__heading">Contact Me</h1>
-            <h2 className="contact__subjeading">You can email me at <span className="contact__at">&#64;</span></h2>
-            <Formik
-                initialValues={{
-                    subject: '',
-                    message: '',
-                }}
-                validationSchema={ContactSchema}
-                >
-                {({ errors, touched }) => (
-                    <Form className="contact-form">
-                        <label className="contact-form__label contact-form__label_subject" htmlFor="subject">Subject</label>
-                        <Field value={subject} onChange={(e) => setSubject(e.target.value)} className="contact-form__input contact-form__input_subject" name="subject" />
-                        <div className={errors.subject && touched.subject ? "contact-form__error contact-form__error_subject" : "contact-form__error contact-form__error_subject transparent"}>{errors.subject}</div>
-                        <label className="contact-form__label contact-form__label_message" htmlFor="message">Message</label>
-                        <Field value={message} onChange={(e) => setMessage(e.target.value)} className="contact-form__input contact-form__input_message" name="message" />
-                        <div className={errors.message && touched.message ? "contact-form__error contact-form__error_message" : "contact-form__error contact-form__error_message transparent"}>{errors.message}</div>
-                        <a
-                            onMouseEnter={ () => handleMail(true) }
-                            onMouseLeave={ () => handleMail(false) }
-                            href={`mailto:${parts[2]}${parts[0]}${parts[3]}${parts[4]}${parts[1]}?subject=${subject}&body=${message}`} 
-                            className="contact-form__button">
-                                Submit
-                        </a>
-                    </Form>
-                )}
-            </Formik>
+            <div className="contact__content-box">
+                <h1 className="contact__heading">Contact Me</h1>
+                <h2 className="contact__subheading"><span className="contact__at">&#64;</span></h2>
+                <Formik
+                    initialValues={{
+                        subject: '',
+                        message: '',
+                    }}
+                    validationSchema={ContactSchema}
+                    >
+                    {({ touched }) => (
+                        <Form className="form">
+                            <label className="form__label form__label_subject" htmlFor="subject">Subject <span className={!subject && touched.subject ? "form__error form__error_subject" : "form__error form__error_subject transparent"}>(required)</span></label>
+                            <Field value={subject} onChange={(e) => setSubject(e.target.value)} className="form__input form__input_subject" name="subject" as="textarea" />
+                            <label className="form__label form__label_message" htmlFor="message">Message <span className={!message && touched.message ? "form__error form__error_message" : "form__error form__error_message transparent"}>(required)</span></label>
+                            <Field value={message} onChange={(e) => setMessage(e.target.value)} className="form__input form__input_message" name="message" as="textarea"/>
+                            <a
+                                onMouseEnter={ () => handleMail(true) }
+                                onMouseLeave={ () => handleMail(false) }
+                                href={subject && message ? `mailto:${parts[2]}${parts[0]}${parts[3]}${parts[4]}${parts[1]}?subject=${subject}&body=${message}` : null} 
+                                className="form__button">
+                                    Send
+                            </a>
+                        </Form>
+                    )}
+                </Formik>
+            </div>
         </section>
     )
 }
