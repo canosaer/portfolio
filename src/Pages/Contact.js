f0f8efa6b50d1451d696d62336951c08314fc098
canosaer <67963828+canosaer@users.noreply.github.com>
2022-05-27 14:04:28 -0400

    Update Contact.js

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -8,6 +8,9 @@ export default function Contact() {
     const [subject, setSubject] = useState('')
     const [message, setMessage] = useState('')
 
+    const firstpart = 'canosaer'
+    const lastpart = 'gmail.com'
+
     const ContactSchema = Yup.object().shape({
         subject: Yup.string().required('Required'),
         message: Yup.string().required('Required'),
@@ -15,6 +18,8 @@ export default function Contact() {
 
     return (
         <section className="contact">
+            <h1 className="contact__heading">Contact Me</h1>
+            <h2 classNAme="contact__subjeading">You can email me at canosaer</h2>
             <Formik
                 initialValues={{
                     subject: '',
@@ -30,7 +35,7 @@ export default function Contact() {
                         <label className="contact-form__label contact-form__label_message" htmlFor="message">Message</label>
                         <Field value={message} onChange={(e) => setMessage(e.target.value)} className="contact-form__input contact-form__input_message" name="message" />
                         <div className={errors.message && touched.message ? "contact-form__error contact-form__error_message" : "contact-form__error contact-form__error_message transparent"}>{errors.message}</div>
-                        <a href={`mailto:canosaer@gmail.com?subject=${subject}&body=${message}`} className="contact-form__button">Submit</a>
+                        <a href={`mailto:${firstpart}@${lastpart}.com?subject=${subject}&body=${message}`} className="contact-form__button">Submit</a>
                     </Form>
                 )}
             </Formik>
